cfffa07ae26f350e0542a52d50c64cf8bbcfc7a1
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-04-21 20:43:59 -0300

    add controller upload_file

---
+++ app/controllers/upload_file_controller.rb
@@ -0,0 +1,20 @@
+class UploadFileController < ApplicationController
+  def new
+  end
+
+  def create
+    begin
+      if params.keys.include?("file")
+        file = params[:file].tempfile.read
+
+        CreatePosts.perform_async(file)
+        redirect_to root_path, alert: "Processing in background!"
+      else
+        raise StandardError.new('File missing!')
+      end
+    rescue StandardError => e
+      flash[:alert] = e.message
+      render :new
+    end
+  end
+end
