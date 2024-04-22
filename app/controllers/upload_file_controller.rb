7aa12966c31011f97c9230501d50768c079336d0
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-04-22 19:38:53 -0300

    remove async

--- app/controllers/upload_file_controller.rb
+++ app/controllers/upload_file_controller.rb
@@ -7,7 +7,7 @@ class UploadFileController < ApplicationController
       if params.keys.include?("file")
         file = params[:file].tempfile.read
 
-        CreatePosts.perform_async(file)
+        ::Posts::Creator.new(file).run!
         redirect_to root_path, alert: "Processing in background!"
       else
         raise StandardError.new('File missing!')
