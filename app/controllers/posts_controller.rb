66fa727e01eaeecfb6bdba7ae728356322cac6ea
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-04-22 16:03:03 -0300

    modifications views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @user = current_user
-    @posts = @user.posts
+    @posts = @user.posts.order(created_at: :desc).page params[:page]
   end
 
   def new
